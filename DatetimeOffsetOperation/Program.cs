00d6a1e6e76f592edcf391f92709d2b0997082b6
SuhutDev <Suhut.Wadiyo@outlook.com>
2023-04-01 06:33:05 +0700

    DatetimeOffset

---
+++ DatetimeOffsetOperation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+var dt = new DateTime(2023, 4, 1, 8, 7, 0);
+var now = DateTime.Now;
+
+System.Console.WriteLine(DateTime.MinValue.Ticks);
+System.Console.WriteLine(DateTime.MinValue.AddSeconds(1).Ticks.ToString("#,##0"));
+
+var jakartaOffset = TimeSpan.FromHours(7);
+var dto = new DateTimeOffset(dt, jakartaOffset);
+
+System.Console.WriteLine($"Offset Time : {dto} ");
+System.Console.WriteLine($"UTC Time : {dto.UtcDateTime} ");
