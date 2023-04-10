c271469b3611dd92377abb40c37416261e994407
Suhut <suhut.wadiyo@gmail.com>
2023-04-10 08:58:16 +0700

    OpenTelemetryJaegerGrafana

---
+++ OpenTelemetryJaegerGrafana/Program.cs
@@ -0,0 +1,60 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(options => options
+        .ConfigureResource(resourceBuilder =>
+        {
+            resourceBuilder.AddService(
+                builder.Environment.ApplicationName,
+                builder.Environment.EnvironmentName,
+                "1.0",
+                false,
+                Environment.MachineName);
+            //resourceBuilder.AddTelemetrySdk();
+        })
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddJaegerExporter()
+
+    );
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/cmdSuccess", async () =>
+{
+    var random = new Random();
+    int dice = random.Next(0, 5);
+    await Task.Delay(dice * 1000);
+     
+    return Results.Ok();
+}).WithOpenApi();
+
+app.MapGet("/cmdFail", async () =>
+{
+    var random = new Random();
+    int dice = random.Next(0, 5);
+    await Task.Delay(dice * 1000); 
+
+    return Results.BadRequest();
+}).WithOpenApi();
+
+app.Run();
+
+ 
