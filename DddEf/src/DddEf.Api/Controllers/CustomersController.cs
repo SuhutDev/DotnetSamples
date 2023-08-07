4a13b0a03ec60944d721e59a9a0ccd9bfe4bf9d2
suhut <suhut.wadiyo@gmail.com>
2023-08-08 06:30:39 +0700

    DddEf

---
+++ DddEf/src/DddEf.Api/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using DddEf.Application.UseCases.Products.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DddEf.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public ProductsController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult<Guid>> Create(CreateProductCommand request)
+        {  
+            return await _sender.Send(request);
+        }
+    }
+}
---
+++ DddEf/src/DddEf.Api/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using DddEf.Application.UseCases.Customers.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DddEf.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public CustomersController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult<Guid>> Create(CreateCustomerCommand request)
+        {  
+            return await _sender.Send(request);
+        }
+    }
+}
